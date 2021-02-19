7b444329919f8011496e4ab51b4a589f0a3fc4a4
Sam Byass <sam.byass@warwick.ac.uk>
2021-02-19 13:59:47 +0000

    Add support for setg - more coming - and remove about 2200 lines of dead code.

---
+++ Cpp2IL/Analysis/Actions/Important/ConditionalRegisterSetAction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Cpp2IL.Analysis.ResultModels;
+using Mono.Cecil.Cil;
+using Instruction = Iced.Intel.Instruction;
+
+namespace Cpp2IL.Analysis.Actions.Important
+{
+    public abstract class ConditionalRegisterSetAction : BaseAction
+    {
+        protected readonly ComparisonAction? _associatedCompare;
+        protected readonly string? _regToSet;
+        private readonly LocalDefinition _localMade;
+
+        public ConditionalRegisterSetAction(MethodAnalysis context, Instruction instruction) : base(context, instruction)
+        {
+            _regToSet = Utils.GetRegisterNameNew(instruction.Op0Register);
+            _associatedCompare = (ComparisonAction?) context.Actions.LastOrDefault(a => a is ComparisonAction);
+
+            _localMade = context.MakeLocal(Utils.BooleanReference, reg: _regToSet);
+        }
+
+        protected abstract string GetTextSummaryCondition();
+        
+        protected abstract string GetPseudocodeCondition();
+
+        public override Mono.Cecil.Cil.Instruction[] ToILInstructions(ILProcessor processor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string? ToPsuedoCode()
+        {
+            return $"{_localMade.Type!.FullName} {_localMade.Name} = {GetPseudocodeCondition()}";
+        }
+
+        public override string ToTextSummary()
+        {
+            return $"[!] Sets the boolean {_localMade} in {_regToSet} to true if {GetTextSummaryCondition()}, otherwise false.";
+        }
+
+        public override bool IsImportant()
+        {
+            return true;
+        }
+    }
+}
---
+++ Cpp2IL/Analysis/Actions/Important/GreaterThanRegisterSetAction.cs
@@ -0,0 +1,22 @@
+﻿using Cpp2IL.Analysis.ResultModels;
+using Iced.Intel;
+
+namespace Cpp2IL.Analysis.Actions.Important
+{
+    public class GreaterThanRegisterSetAction : ConditionalRegisterSetAction
+    {
+        public GreaterThanRegisterSetAction(MethodAnalysis context, Instruction instruction) : base(context, instruction)
+        {
+        }
+
+        protected override string GetTextSummaryCondition()
+        {
+            return $"{_associatedCompare?.ArgumentOne?.GetPseudocodeRepresentation()} is greater than {_associatedCompare?.ArgumentTwo?.GetPseudocodeRepresentation()}";
+        }
+
+        protected override string GetPseudocodeCondition()
+        {
+            return $"{_associatedCompare?.ArgumentOne?.GetPseudocodeRepresentation()} > {_associatedCompare?.ArgumentTwo?.GetPseudocodeRepresentation()}";
+        }
+    }
+}
