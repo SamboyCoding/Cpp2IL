a179bdbc07043337a66b922cab8f77a2f5a69150
ds5678 <jrpritts7@gmail.com>
2021-07-23 17:04:33 -0400

    Added some logging overrides

--- Cpp2IL.Core/Logger.cs
+++ Cpp2IL.Core/Logger.cs
@@ -11,40 +11,56 @@ namespace Cpp2IL.Core
         internal static readonly Color INFO = Color.LightBlue;
         internal static readonly Color WARN = Color.Yellow;
         internal static readonly Color ERROR = Color.DarkRed;
-        
+
+        public static Action<string, string>? VerboseOverride { get; set; }
+        public static Action<string, string>? InfoOverride { get; set; }
+        public static Action<string, string>? WarnOverride { get; set; }
+        public static Action<string, string>? ErrorOverride { get; set; }
+
         internal static bool DisableColor { private get; set; }
 
         internal static bool ShowVerbose { private get; set; }
         
         private static bool LastNoNewline;
 
-        public static void VerboseNewline(string message, string source = "Program") => Verbose($"{message}\n", source);
+        public static void VerboseNewline(string message, string source = "Program") => Verbose($"{message}{Environment.NewLine}", source);
 
         public static void Verbose(string message, string source = "Program")
         {
-            if(ShowVerbose)
+            if (VerboseOverride != null) 
+                VerboseOverride.Invoke(message, source);
+            else if(ShowVerbose)
                 Write("Verb", source, message, VERB);
         }
         
-        public static void InfoNewline(string message, string source = "Program") => Info($"{message}\n", source);
+        public static void InfoNewline(string message, string source = "Program") => Info($"{message}{Environment.NewLine}", source);
 
         public static void Info(string message, string source = "Program")
         {
-            Write("Info", source, message, INFO);
+            if (InfoOverride != null) 
+                InfoOverride.Invoke(message, source);
+            else 
+                Write("Info", source, message, INFO);
         }
         
-        public static void WarnNewline(string message, string source = "Program") => Warn($"{message}\n", source);
+        public static void WarnNewline(string message, string source = "Program") => Warn($"{message}{Environment.NewLine}", source);
 
         public static void Warn(string message, string source = "Program")
         {
-            Write("Warn", source, message, WARN);
+            if (WarnOverride != null) 
+                WarnOverride.Invoke(message, source);
+            else 
+                Write("Warn", source, message, WARN);
         }
         
-        public static void ErrorNewline(string message, string source = "Program") => Error($"{message}\n", source);
+        public static void ErrorNewline(string message, string source = "Program") => Error($"{message}{Environment.NewLine}", source);
 
         public static void Error(string message, string source = "Program")
         {
-            Write("Fail", source, message, ERROR);
+            if (ErrorOverride != null) 
+                ErrorOverride.Invoke(message, source);
+            else 
+                Write("Fail", source, message, ERROR);
         }
 
         internal static void Write(string level, string source, string message, Color color)
