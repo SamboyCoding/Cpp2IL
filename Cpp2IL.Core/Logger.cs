6486947ba7728f59a57b18129ea7e158c6e480fd
Bepis <36346617+bbepis@users.noreply.github.com>
2021-08-07 21:26:31 +1000

    Initialize log events with empty handler

--- Cpp2IL.Core/Logger.cs
+++ Cpp2IL.Core/Logger.cs
@@ -6,37 +6,37 @@ namespace Cpp2IL.Core
     {
 	    public delegate void LogEvent(string message, string source);
 
-	    public static event LogEvent VerboseLog;
-	    public static event LogEvent InfoLog;
-	    public static event LogEvent WarningLog;
-	    public static event LogEvent ErrorLog;
+	    public static event LogEvent VerboseLog = (_, _) => {};
+	    public static event LogEvent InfoLog = (_, _) => { };
+        public static event LogEvent WarningLog = (_, _) => { };
+        public static event LogEvent ErrorLog = (_, _) => { };
 
         public static void VerboseNewline(string message, string source = "Program") => Verbose($"{message}{Environment.NewLine}", source);
 
         public static void Verbose(string message, string source = "Program")
         {
-            VerboseLog?.Invoke(message, source);
+            VerboseLog(message, source);
         }
         
         public static void InfoNewline(string message, string source = "Program") => Info($"{message}{Environment.NewLine}", source);
 
         public static void Info(string message, string source = "Program")
         {
-            InfoLog?.Invoke(message, source);
+            InfoLog(message, source);
         }
         
         public static void WarnNewline(string message, string source = "Program") => Warn($"{message}{Environment.NewLine}", source);
 
         public static void Warn(string message, string source = "Program")
         {
-	        WarningLog?.Invoke(message, source);
+	        WarningLog(message, source);
         }
         
         public static void ErrorNewline(string message, string source = "Program") => Error($"{message}{Environment.NewLine}", source);
 
         public static void Error(string message, string source = "Program")
         {
-            ErrorLog?.Invoke(message, source);
+            ErrorLog(message, source);
         }
     }
 }
