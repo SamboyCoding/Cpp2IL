016d2ee62e0508177b230beeed16f720bc791f13
Sam Byass <sam.byass@warwick.ac.uk>
2021-09-05 11:30:54 +0100

    Abstract out newobj action

---
+++ Cpp2IL.Core/Analysis/Actions/Base/AbstractNewObjAction.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using Cpp2IL.Core.Analysis.Actions.x86;
+using Cpp2IL.Core.Analysis.ResultModels;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Instruction = Iced.Intel.Instruction;
+
+namespace Cpp2IL.Core.Analysis.Actions.Base
+{
+    public abstract class AbstractNewObjAction<T> : BaseAction<T>
+    {
+        public TypeReference? TypeCreated;
+        public LocalDefinition? LocalReturned;
+        
+        protected AbstractNewObjAction(MethodAnalysis context, T associatedInstruction) : base(context, associatedInstruction)
+        {
+        }
+        
+        public override Mono.Cecil.Cil.Instruction[] ToILInstructions(MethodAnalysis context, ILProcessor processor)
+        {
+            if (LocalReturned == null || TypeCreated == null)
+                throw new TaintedInstructionException("Local being returned, or type being allocated, couldn't be determined.");
+            
+            //TODO Once we abstract out MethodAnalysis somehow, fix this.
+            //This is super ugly until that point.
+            var thisAsX86Action = (BaseAction<Instruction>)(object)this;
+            
+            var managedConstructorCall = (BaseX86CallAction?) context.Actions.Skip(context.Actions.IndexOf(thisAsX86Action)).FirstOrDefault(i => i is BaseX86CallAction);
+
+            if (managedConstructorCall == null)
+                throw new TaintedInstructionException($"Cannot find the call to the constructor for instance allocation, of type {TypeCreated} (Is Value: {TypeCreated?.IsValueType})");
+
+            //Next call should be to a constructor.
+            if (managedConstructorCall.ManagedMethodBeingCalled?.Name != ".ctor")
+                throw new TaintedInstructionException($"Managed method being called is {managedConstructorCall.ManagedMethodBeingCalled?.Name}, not a ctor.");
+
+            var result = managedConstructorCall.GetILToLoadParams(context, processor, false);
+
+            var ctorToCall = managedConstructorCall.ManagedMethodBeingCalled!;
+            
+            if (ctorToCall.DeclaringType != TypeCreated)
+                ctorToCall = TypeCreated?.Resolve()?.Methods.FirstOrDefault(m => m.Name == ".ctor" && m.Parameters.Count == ctorToCall.Parameters.Count) ?? throw new TaintedInstructionException($"Could not resolve a constructor with {ctorToCall.Parameters.Count} parameters.");
+
+            if (ctorToCall.HasGenericParameters && TypeCreated is GenericInstanceType git)
+                ctorToCall = ctorToCall.MakeGeneric(git.GenericArguments.ToArray());
+
+            result.Add(processor.Create(OpCodes.Newobj, processor.ImportReference(ctorToCall)));
+            
+            result.Add(processor.Create(OpCodes.Stloc, LocalReturned.Variable));
+
+            return result.ToArray();
+        }
+
+        public override string ToPsuedoCode()
+        {
+            return $"{TypeCreated?.FullName} {LocalReturned?.Name} = new {TypeCreated?.FullName}()";
+        }
+
+        public override string ToTextSummary()
+        {
+            return $"[!] Allocates an instance of type {TypeCreated} and stores it as {LocalReturned?.Name} in rax.\n";
+        }
+        
+        public override bool IsImportant()
+        {
+            return true;
+        }
+    }
+}
--- Cpp2IL.Core/Analysis/Actions/Important/AllocateInstanceAction.cs
+++ Cpp2IL.Core/Analysis/Actions/Important/AllocateInstanceAction.cs
@@ -10,11 +10,8 @@ using Instruction = Iced.Intel.Instruction;
 
 namespace Cpp2IL.Core.Analysis.Actions.Important
 {
-    public class AllocateInstanceAction : BaseAction<Instruction>
+    public class AllocateInstanceAction : AbstractNewObjAction<Instruction>
     {
-        public TypeReference? TypeCreated;
-        public LocalDefinition? LocalReturned;
-        
         public AllocateInstanceAction(MethodAnalysis context, Instruction instruction) : base(context, instruction)
         {
             var constant = !LibCpp2IlMain.Binary!.is32Bit ? context.GetConstantInReg("rcx") : context.Stack.Peek() as ConstantDefinition;
@@ -40,52 +37,6 @@ namespace Cpp2IL.Core.Analysis.Actions.Important
             TypeCreated = typeCreated;
             
             LocalReturned = context.MakeLocal(TypeCreated, reg: "rax");
-        }
-
-        public override Mono.Cecil.Cil.Instruction[] ToILInstructions(MethodAnalysis context, ILProcessor processor)
-        {
-            if (LocalReturned == null || TypeCreated == null)
-                throw new TaintedInstructionException("Local being returned, or type being allocated, couldn't be determined.");
-            
-            var managedConstructorCall = (BaseX86CallAction) context.Actions.Skip(context.Actions.IndexOf(this)).FirstOrDefault(i => i is BaseX86CallAction);
-
-            if (managedConstructorCall == null)
-                throw new TaintedInstructionException($"Cannot find the call to the constructor for instance allocation, of type {TypeCreated} (Is Value: {TypeCreated?.IsValueType})");
-
-            //Next call should be to a constructor.
-            if (managedConstructorCall.ManagedMethodBeingCalled?.Name != ".ctor")
-                throw new TaintedInstructionException($"Managed method being called is {managedConstructorCall.ManagedMethodBeingCalled?.Name}, not a ctor.");
-
-            var result = managedConstructorCall.GetILToLoadParams(context, processor, false);
-
-            var ctorToCall = managedConstructorCall.ManagedMethodBeingCalled!;
-            
-            if (ctorToCall.DeclaringType != TypeCreated)
-                ctorToCall = TypeCreated?.Resolve()?.Methods.FirstOrDefault(m => m.Name == ".ctor" && m.Parameters.Count == ctorToCall.Parameters.Count) ?? throw new TaintedInstructionException($"Could not resolve a constructor with {ctorToCall.Parameters.Count} parameters.");
-
-            if (ctorToCall.HasGenericParameters && TypeCreated is GenericInstanceType git)
-                ctorToCall = ctorToCall.MakeGeneric(git.GenericArguments.ToArray());
-
-            result.Add(processor.Create(OpCodes.Newobj, processor.ImportReference(ctorToCall)));
-            
-            result.Add(processor.Create(OpCodes.Stloc, LocalReturned.Variable));
-
-            return result.ToArray();
-        }
-
-        public override string ToPsuedoCode()
-        {
-            return $"{TypeCreated?.FullName} {LocalReturned?.Name} = new {TypeCreated?.FullName}()";
-        }
-
-        public override string ToTextSummary()
-        {
-            return $"[!] Allocates an instance of type {TypeCreated} and stores it as {LocalReturned?.Name} in rax.\n";
-        }
-        
-        public override bool IsImportant()
-        {
-            return true;
         }
     }
 }
