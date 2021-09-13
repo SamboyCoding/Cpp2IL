1a7f3ce7db7941ede02c91dd0f742623825f6bb6
Sam Byass <sam.byass@warwick.ac.uk>
2021-09-13 19:22:09 +0100

    Abstract out array allocation

---
+++ Cpp2IL.Core/Analysis/Actions/Base/AbstractArrayAllocationAction.cs
@@ -0,0 +1,52 @@
+﻿using Cpp2IL.Core.Analysis.ResultModels;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Cpp2IL.Core.Analysis.Actions.Base
+{
+    public abstract class AbstractArrayAllocationAction<T> : BaseAction<T>
+    {
+        protected int SizeAllocated;
+        protected TypeReference? TypeOfArray;
+        protected LocalDefinition? LocalWritten;
+
+        protected AbstractArrayAllocationAction(MethodAnalysis<T> context, T associatedInstruction) : base(context, associatedInstruction)
+        {
+        }
+
+        public override Mono.Cecil.Cil.Instruction[] ToILInstructions(MethodAnalysis<T> context, ILProcessor processor)
+        {
+            if (LocalWritten == null || TypeOfArray == null)
+                throw new TaintedInstructionException("Missing created local or type of array");
+
+            if (TypeOfArray is not ArrayType arrayType)
+                throw new TaintedInstructionException("Array type isn't an array");
+
+            return new []
+            {
+                processor.Create(OpCodes.Ldc_I4, SizeAllocated),
+                processor.Create(OpCodes.Newarr, processor.ImportReference(arrayType.ElementType)),
+                processor.Create(OpCodes.Stloc, LocalWritten.Variable)
+            };
+        }
+
+        public override string? ToPsuedoCode()
+        {
+            var aType = TypeOfArray as ArrayType;
+            return $"{TypeOfArray?.FullName} {LocalWritten?.Name} = new {aType?.ElementType}[{SizeAllocated}]";
+        }
+
+        public override string ToTextSummary()
+        {
+            if (!(TypeOfArray is ArrayType))
+                return $"[!!] Allocates an array of a type which isn't an array (got {TypeOfArray}), of size {SizeAllocated}, and stores the result as {LocalWritten?.Name}. This is a problem - we couldn't resolve the array type";
+            
+            return $"[!] Allocates an array of type {TypeOfArray?.FullName} of size {SizeAllocated} and stores the result as {LocalWritten?.Name} in register rax\n";
+        }
+
+        public override bool IsImportant()
+        {
+            return true;
+        }
+    }
+}
--- Cpp2IL.Core/Analysis/Actions/Important/AllocateArrayAction.cs
+++ Cpp2IL.Core/Analysis/Actions/Important/AllocateArrayAction.cs
@@ -8,12 +8,8 @@ using Instruction = Iced.Intel.Instruction;
 
 namespace Cpp2IL.Core.Analysis.Actions.Important
 {
-    public class AllocateArrayAction : BaseAction<Instruction>
+    public class AllocateArrayAction : AbstractArrayAllocationAction<Instruction>
     {
-        private readonly int sizeAllocated;
-        private readonly TypeReference? typeOfArray;
-        private readonly LocalDefinition? _localWritten;
-
         public AllocateArrayAction(MethodAnalysis<Instruction> context, Instruction instruction) : base(context, instruction)
         {
             var typeConstant = !LibCpp2IlMain.Binary!.is32Bit ? context.GetConstantInReg("rcx") : context.Stack.Peek() as ConstantDefinition;
@@ -32,61 +28,26 @@ namespace Cpp2IL.Core.Analysis.Actions.Important
 
             if (typeConstant.Value is TypeReference reference)
             {
-                typeOfArray = reference;
+                TypeOfArray = reference;
             }
 
             if (sizeOperand is LocalDefinition local && (local.KnownInitialValue is ulong || local.KnownInitialValue is uint))
             {
                 RegisterUsedLocal(local);
-                sizeAllocated = Convert.ToInt32(local.KnownInitialValue);
+                SizeAllocated = Convert.ToInt32(local.KnownInitialValue);
             }
             else if (sizeOperand is ConstantDefinition {Value: ulong sizeC})
             {
-                sizeAllocated = (int) sizeC;
+                SizeAllocated = (int) sizeC;
             } else if (sizeOperand is ConstantDefinition {Value: uint sizeCSmall})
             {
-                sizeAllocated = (int) sizeCSmall;
+                SizeAllocated = (int) sizeCSmall;
             }
 
-            if (!(typeOfArray is ArrayType arrayType)) return;
+            if (TypeOfArray is not ArrayType arrayType) return;
 
-            _localWritten = context.MakeLocal(arrayType, reg: "rax", knownInitialValue: new AllocatedArray(sizeAllocated, arrayType));
-            RegisterUsedLocal(_localWritten); //Used implicitly until I can find out what's causing these issues
-        }
-
-        public override Mono.Cecil.Cil.Instruction[] ToILInstructions(MethodAnalysis<Instruction> context, ILProcessor processor)
-        {
-            if (_localWritten == null || typeOfArray == null)
-                throw new TaintedInstructionException("Missing created local or type of array");
-
-            if (!(typeOfArray is ArrayType arrayType))
-                throw new TaintedInstructionException("Array type isn't an array");
-
-            return new []
-            {
-                processor.Create(OpCodes.Ldc_I4, sizeAllocated),
-                processor.Create(OpCodes.Newarr, processor.ImportReference(arrayType.ElementType)),
-                processor.Create(OpCodes.Stloc, _localWritten.Variable)
-            };
-        }
-
-        public override string? ToPsuedoCode()
-        {
-            var aType = typeOfArray as ArrayType;
-            return $"{typeOfArray?.FullName} {_localWritten?.Name} = new {aType?.ElementType}[{sizeAllocated}]";
-        }
-
-        public override string ToTextSummary()
-        {
-            if (!(typeOfArray is ArrayType))
-                return $"[!!] Allocates an array of a type which isn't an array (got {typeOfArray}), of size {sizeAllocated}, and stores the result as {_localWritten?.Name}. This is a problem - we couldn't resolve the array type";
-            
-            return $"[!] Allocates an array of type {typeOfArray?.FullName} of size {sizeAllocated} and stores the result as {_localWritten?.Name} in register rax\n";
-        }
-
-        public override bool IsImportant()
-        {
-            return true;
+            LocalWritten = context.MakeLocal(arrayType, reg: "rax", knownInitialValue: new AllocatedArray(SizeAllocated, arrayType));
+            RegisterUsedLocal(LocalWritten); //Used implicitly until I can find out what's causing these issues
         }
     }
 }
