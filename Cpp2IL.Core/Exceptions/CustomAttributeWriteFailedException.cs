afdad0e721cc43d86dd283ef30c2df991e75e593
Sam Byass <sam.byass@warwick.ac.uk>
2021-11-01 19:14:59 +0000

    Give custom attribute a better exception

--- Cpp2IL.Core/Exceptions/CustomAttributeWriteFailedException.cs
+++ Cpp2IL.Core/Exceptions/CustomAttributeWriteFailedException.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Linq;
 using Mono.Cecil;
 
 namespace Cpp2IL.Core.Exceptions
 {
     public class CustomAttributeWriteFailedException : Exception
     {
-        public CustomAttributeWriteFailedException(CustomAttribute customAttribute, Exception cause) : base($"Failed to write custom attribute {customAttribute} due to an exception", cause)
+        public CustomAttributeWriteFailedException(CustomAttribute customAttribute, Exception cause) 
+            : base($"Failed to write custom attribute {customAttribute.Constructor.DeclaringType} with arguments {string.Join(", ", customAttribute.ConstructorArguments.Select(a => a.Value + " of type " + a.Type))} due to an exception", cause)
         { }
     }
 }
