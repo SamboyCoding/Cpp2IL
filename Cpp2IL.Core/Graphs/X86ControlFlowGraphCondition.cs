9f38e4134526334d080d852c99ec06ac7d289120
gompoc <91314780+gompoc@users.noreply.github.com>
2021-11-29 18:14:58 +0000

    Correct formatting for test comparison and separate class into separate file

---
+++ Cpp2IL.Core/Graphs/X86ControlFlowGraphCondition.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Iced.Intel;
+
+namespace Cpp2IL.Core;
+
+public class X86ControlFlowGraphCondition : Condition<Instruction>
+{
+    private static MasmFormatter _formatter = new();
+    private static StringOutput _output = new();
+    public X86ControlFlowGraphCondition(Instruction comparison, Instruction conditionalJump) : base(comparison, conditionalJump)
+    {
+    }
+
+    public override string GetCondition()
+    {
+        if (Comparison.Mnemonic == Mnemonic.Test)
+        {
+            if (Comparison.Op0Register == Comparison.Op1Register)
+            {
+                _formatter.FormatOperand(Comparison, _output, 0);
+                return $"{_output.ToStringAndReset()} {GetConditionOperator()} 0";
+            }
+            _formatter.FormatOperand(Comparison, _output, 0);
+            var argumentOne = _output.ToStringAndReset();
+            _formatter.FormatOperand(Comparison, _output, 1);
+            var argumentTwo = _output.ToStringAndReset();
+            return $"({argumentOne} & {argumentTwo}) {GetConditionOperator()} 0";
+        }
+        if(Comparison.Mnemonic == Mnemonic.Cmp)
+        {
+            _formatter.FormatOperand(Comparison, _output, 0);
+            var argumentOne = _output.ToStringAndReset();
+            _formatter.FormatOperand(Comparison, _output, 1);
+            var argumentTwo = _output.ToStringAndReset();
+            return $"{argumentOne} {GetConditionOperator()} {argumentTwo}";
+        }
+        throw new Exception($"Don't know what to do with {Comparison.Mnemonic}");
+    }
+
+    public override string GetConditionOperator()
+    {
+        switch (Jump.Mnemonic)
+        {
+            case Mnemonic.Je:
+                return "==";
+            case Mnemonic.Jne:
+                return "!=";
+            case Mnemonic.Jg:
+                return ">";
+            case Mnemonic.Jge:
+                return ">=";
+            case Mnemonic.Jl:
+                return "<";
+            case Mnemonic.Jle:
+                return "<=";
+            case Mnemonic.Ja:
+                return ">";
+            case Mnemonic.Jae:
+                return ">=";
+            case Mnemonic.Jb:
+                return "<";
+            case Mnemonic.Jbe:
+                return "<=";
+            default:
+                throw new Exception($"{Jump.Mnemonic} isn't supported currently");
+        }
+    }
+}
