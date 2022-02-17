469e2f528d635b5a0c2b0c70782d5f3932dbfe19
js6pak <kubastaron@hotmail.com>
2022-02-17 17:48:22 +0100

    Revert "Update WasmInstruction.cs"
    This reverts commit fee377e6cf0eebf170b6f72eb895092165e68fc2.

--- WasmDisassembler/WasmInstruction.cs
+++ WasmDisassembler/WasmInstruction.cs
@@ -6,11 +6,6 @@ public struct WasmInstruction
     public uint NextIp;
     public WasmMnemonic Mnemonic;
     public object[] Operands = Array.Empty<object>();
-    
-    public WasmInstruction() 
-    {
-        //Required constructor bc microsoft silently broke structs   
-    }
 
     public override string ToString()
     {
