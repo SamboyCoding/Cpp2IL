0d89ca3e79ddb32ea200a43ab0e7aabf7cf5ac4a
Sam Byass <sam.byass@warwick.ac.uk>
2022-05-08 17:34:08 +0100

    Cpp2IL: Fix simple-attribute-restoration not being applied if analysis is enabled

--- Cpp2IL/Program.cs
+++ Cpp2IL/Program.cs
@@ -406,7 +406,7 @@ namespace Cpp2IL
             Logger.InfoNewline($"Applying type, method, and field attributes for {Cpp2IlApi.GeneratedAssemblies.Count} assemblies...This may take a couple of seconds");
             var start = DateTime.Now;
 
-            Cpp2IlApi.RunAttributeRestorationForAllAssemblies(keyFunctionAddresses, parallel: LibCpp2IlMain.MetadataVersion >= 29 || LibCpp2IlMain.Binary!.InstructionSet is InstructionSet.X86_32 or InstructionSet.X86_64);
+            Cpp2IlApi.RunAttributeRestorationForAllAssemblies(runtimeArgs.SimpleAttributeRestoration ? null : keyFunctionAddresses, parallel: LibCpp2IlMain.MetadataVersion >= 29 || LibCpp2IlMain.Binary!.InstructionSet is InstructionSet.X86_32 or InstructionSet.X86_64);
 
             Logger.InfoNewline($"Finished Applying Attributes in {(DateTime.Now - start).TotalMilliseconds:F0}ms");
 
