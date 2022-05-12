92cdc2523a1ab11c7c58975c1c752d6c38527a92
Sam Byass <sam.byass@warwick.ac.uk>
2022-05-12 13:30:43 +0100

    Core: Make initial loading of arm64 binaries faster by not trying to disassemble

--- Cpp2IL.Core/CorePlugin/Arm64InstructionSet.cs
+++ Cpp2IL.Core/CorePlugin/Arm64InstructionSet.cs
@@ -8,6 +8,7 @@ using Cpp2IL.Core.Il2CppApiFunctions;
 using Cpp2IL.Core.ISIL;
 using Cpp2IL.Core.Model.Contexts;
 using Cpp2IL.Core.Utils;
+using LibCpp2IL;
 
 namespace Cpp2IL.Core.CorePlugin;
 
@@ -20,6 +21,18 @@ public class Arm64InstructionSet : Cpp2IlInstructionSet
 
     public override Memory<byte> GetRawBytesForMethod(MethodAnalysisContext context, bool isAttributeGenerator)
     {
+        //Avoid use of capstone where possible
+        if (context is not ConcreteGenericMethodAnalysisContext)
+        {
+            //Managed method or attr gen => grab raw byte range between a and b
+            var startOfNextFunction = (int) MiscUtils.GetAddressOfNextFunctionStart(context.UnderlyingPointer) - 1;
+            var ptrAsInt = (int) context.UnderlyingPointer;
+            var count = startOfNextFunction - ptrAsInt;
+
+            if (startOfNextFunction > 0)
+                return LibCpp2IlMain.Binary!.GetRawBinaryContent().AsMemory(ptrAsInt, count);
+        }
+        
         var instructions = Arm64Utils.GetArm64MethodBodyAtVirtualAddress(context.UnderlyingPointer);
 
         return instructions.SelectMany(i => i.Bytes).ToArray();
