1590d32b6cffd9122829d7790526f541b6fb09c2
Alluseri <42897348+Alluseri@users.noreply.github.com>
2022-08-24 14:51:52 +0500

    GUI: Added support for dropped single apk files

--- Cpp2IL.Gui/Views/MainWindow.axaml.cs
+++ Cpp2IL.Gui/Views/MainWindow.axaml.cs
@@ -12,61 +12,60 @@ using Cpp2IL.Gui.ViewModels;
 
 namespace Cpp2IL.Gui.Views
 {
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
 #if DEBUG
-			this.AttachDevTools();
+            this.AttachDevTools();
 #endif
-		}
+        }
 
-		public void OnCreated()
-		{
-			var vm = (MainWindowViewModel)DataContext!;
-			vm.Window = this;
+        public void OnCreated()
+        {
+            var vm = (MainWindowViewModel) DataContext!;
+            vm.Window = this;
+            
+            var textEditor = this.FindControl<TextEditor>("CodeView");
 
-			var textEditor = this.FindControl<TextEditor>("CodeView");
+            var registryOptions = new RegistryOptions(ThemeName.DarkPlus);
+            var textMateInstallation = textEditor.InstallTextMate(registryOptions);
+            textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId(registryOptions.GetLanguageByExtension(".cs").Id));
+            
+            // This assumes nothing else is passed to the command line as it is a GUI build!
+            var commandLine = System.Environment.GetCommandLineArgs();
+            if (commandLine != null && commandLine.Length > 1) {
+                vm.OnDropped(commandLine.Skip(1).ToArray());
+            }
+            AddHandler(DragDrop.DropEvent, (sender, args) => vm.OnDropped(args.Data.GetFileNames()?.ToArray()));
+        }
 
-			var registryOptions = new RegistryOptions(ThemeName.DarkPlus);
-			var textMateInstallation = textEditor.InstallTextMate(registryOptions);
-			textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId(registryOptions.GetLanguageByExtension(".cs").Id));
+        private async void OnClickDropPrompt(object? sender, PointerPressedEventArgs e)
+        {
+            OpenFileDialog dialog = new()
+            {
+                AllowMultiple = true
+            };
+            var ret = await dialog.ShowAsync(this);
+            var vm = (MainWindowViewModel) DataContext!;
+            vm.OnDropped(ret);
+        }
 
-			// This assumes nothing else is passed to the command line as it is a GUI build!
-			var commandLine = System.Environment.GetCommandLineArgs();
-			if (commandLine != null && commandLine.Length > 1)
-			{
-				vm.OnDropped(commandLine.Skip(1).ToArray());
-			}
-			AddHandler(DragDrop.DropEvent, (sender, args) => vm.OnDropped(args.Data.GetFileNames()?.ToArray()));
-		}
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
 
-		private async void OnClickDropPrompt(object? sender, PointerPressedEventArgs e)
-		{
-			OpenFileDialog dialog = new()
-			{
-				AllowMultiple = true
-			};
-			var ret = await dialog.ShowAsync(this);
-			var vm = (MainWindowViewModel)DataContext!;
-			vm.OnDropped(ret);
-		}
+        private void SelectingItemsControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count < 1)
+                return;
+            
+            if(e.AddedItems[0] is not FileTreeEntry fte)
+                return;
 
-		private void InitializeComponent()
-		{
-			AvaloniaXamlLoader.Load(this);
-		}
-
-		private void SelectingItemsControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
-		{
-			if (e.AddedItems.Count < 1)
-				return;
-
-			if (e.AddedItems[0] is not FileTreeEntry fte)
-				return;
-
-			((MainWindowViewModel)DataContext!).OnItemSelected(fte);
-		}
-	}
+            ((MainWindowViewModel) DataContext!).OnItemSelected(fte);
+        }
+    }
 }
