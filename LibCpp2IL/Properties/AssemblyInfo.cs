fc8f02a72e69e53e36c34db665986e97c8fb1e52
Sam Byass <sam@samboycoding.me>
2022-09-26 00:59:32 +0100

    Bump core, lib, and wrapper to 2022.0.6 in prep for release

--- LibCpp2IL/Properties/AssemblyInfo.cs
+++ LibCpp2IL/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Samboy063 2019-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2022.0.5")]
-[assembly: AssemblyFileVersion("2022.0.5")]
+[assembly: AssemblyVersion("2022.0.6")]
+[assembly: AssemblyFileVersion("2022.0.6")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
