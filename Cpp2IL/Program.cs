43dd2529d564e2fb0d1d682225984c0b9db010e5
Sam Byass <sam@samboycoding.me>
2023-01-30 01:19:03 +0000

    Cpp2IL: Read version from UnityPlayer dll instead of player exe to fix 2021.2 bug

--- Cpp2IL/Program.cs
+++ Cpp2IL/Program.cs
@@ -62,14 +62,19 @@ namespace Cpp2IL
             if (exeName == null)
                 throw new SoftException("Failed to locate any executable in the provided game directory. Make sure the path is correct, and if you *really* know what you're doing (and know it's not supported), use the force options, documented if you provide --help.");
 
-            var unityPlayerPath = Path.Combine(gamePath, $"{exeName}.exe");
+            var playerExePath = Path.Combine(gamePath, $"{exeName}.exe");
             args.PathToMetadata = Path.Combine(gamePath, $"{exeName}_Data", "il2cpp_data", "Metadata", "global-metadata.dat");
 
-            if (!File.Exists(args.PathToAssembly) || !File.Exists(unityPlayerPath) || !File.Exists(args.PathToMetadata))
+            if (!File.Exists(args.PathToAssembly) || !File.Exists(playerExePath) || !File.Exists(args.PathToMetadata))
                 throw new SoftException("Invalid game-path or exe-name specified. Failed to find one of the following:\n" +
                                         $"\t{args.PathToAssembly}\n" +
-                                        $"\t{unityPlayerPath}\n" +
+                                        $"\t{playerExePath}\n" +
                                         $"\t{args.PathToMetadata}\n");
+
+            var unityPlayerPath = Path.Combine(gamePath, "UnityPlayer.dll");
+
+            if (!File.Exists(unityPlayerPath))
+                unityPlayerPath = playerExePath;
 
             Logger.VerboseNewline($"Found probable windows game at path: {gamePath}. Attempting to get unity version...");
             var gameDataPath = Path.Combine(gamePath, $"{exeName}_Data");
