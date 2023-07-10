091cdc21c9ca5318afbfa38916767d560ab1e662
Sam Byass <sam@samboycoding.me>
2023-07-10 11:36:06 +0100

    SCR: Support multiple CGM list addresses

--- Cpp2IL.Plugin.StrippedCodeRegSupport/StrippedCodeRegSupportPlugin.cs
+++ Cpp2IL.Plugin.StrippedCodeRegSupport/StrippedCodeRegSupportPlugin.cs
@@ -49,19 +49,55 @@ public class StrippedCodeRegSupportPlugin : Cpp2IlPlugin
 
         //This gives us the address of the CodeGenModule struct for mscorlib.dll, which is *somewhere* in the CodeGenModules list - near the end, but rarely AT the end.
         var pMscorlibCodegenEntryInCodegenModulesList = searcher.FindAllMappedWords(pMscorlibCodegenModule).ToList(); //CodeGenModules list address will be in here
+        
+        var moduleCount = (ulong) metadata.AssemblyDefinitions.Length;
 
-        if (pMscorlibCodegenEntryInCodegenModulesList.Count != 1)
+        for (var i = 0; i < pMscorlibCodegenEntryInCodegenModulesList.Count; i++)
         {
-            Logger.ErrorNewline($"Found {pMscorlibCodegenEntryInCodegenModulesList.Count} potential CodeGenModules lists for mscorlib, expected 1. Cannot continue. If this is more than one, I can optionally add support if requested.");
+            var pSomewhereInCodegenModules = pMscorlibCodegenEntryInCodegenModulesList[i];
+
+            var startOfCodegenModulesList = FindCodeGenModulesListFromMscorlib(binary, pSomewhereInCodegenModules, moduleCount, out var endOfCodegenModulesList);
+            
+            //Try to read the first entry as a codegen module pointer.
+            var firstModulePtr = binary.ReadPointerAtVirtualAddress(startOfCodegenModulesList);
+            if (firstModulePtr == 0)
+            {
+                Logger.VerboseNewline($"Found start of CodeGenModules list at 0x{startOfCodegenModulesList:X}, but first entry is null. Continuing search...");
+                continue;
+            }
+            
+            //Now try to read that as a CGM
+            Il2CppCodeGenModule? firstModule = null;
+            try
+            {
+                firstModule = binary.ReadReadableAtVirtualAddress<Il2CppCodeGenModule>(firstModulePtr);
+                if (firstModule?.Name is not {} name || string.IsNullOrWhiteSpace(name) || name.Any(c => !char.IsAscii(c)))
+                {
+                    Logger.VerboseNewline($"Discarding CodeGenModules list at 0x{startOfCodegenModulesList:X} because first module name is invalid.");
+                    continue;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.VerboseNewline($"Discarding CodeGenModules list at 0x{startOfCodegenModulesList:X} because we hit a {e.GetType().Name} when trying to read it as a CGM array.");
+                continue;
+            }
+
+            Logger.VerboseNewline($"Found potential CodeGenModules list at 0x{startOfCodegenModulesList:X} (ending at 0x{endOfCodegenModulesList:X}), with valid first module name {firstModule.Name}. Returning dummy code reg struct now!");
+
+            //Now we can return a dummy CodeRegistration struct with the correct values.
+            codereg = new() { codeGenModulesCount = (uint)moduleCount, addrCodeGenModulePtrs = startOfCodegenModulesList };
+            
             return;
         }
-        
-        //But unlike what BinarySearcher does now, we can't walk back and keep searching for references. But we know how many modules there are:
-        var moduleCount = (ulong) metadata.AssemblyDefinitions.Length;
-        
+    }
+
+    private static ulong FindCodeGenModulesListFromMscorlib(Il2CppBinary binary, ulong pSomewhereInCodegenModules, ulong moduleCount, out ulong endOfCodegenModulesList)
+    {
+        //Unlike what BinarySearcher does now, we can't walk back and keep searching for references. But we know how many modules there are,
         //So let's now read *forward* one pointer at a time until we hopefully hit a null pointer.
         var pointerSize = binary.PointerSize;
-        var endOfCodegenModulesList = pMscorlibCodegenEntryInCodegenModulesList[0] + pointerSize;
+        endOfCodegenModulesList = pSomewhereInCodegenModules + pointerSize;
         binary.Position = binary.MapVirtualAddressToRaw(endOfCodegenModulesList);
 
         while (binary.ReadNUint() != 0)
@@ -69,16 +105,12 @@ public class StrippedCodeRegSupportPlugin : Cpp2IlPlugin
             endOfCodegenModulesList += pointerSize;
             binary.Position += (long)pointerSize;
         }
-        
+
         //We're at the end, so walk one back to get the last valid pointer.
         endOfCodegenModulesList -= pointerSize;
 
         //Now subtract module count * pointer size to get the start of the list.
         var startOfCodegenModulesList = endOfCodegenModulesList - ((moduleCount - 1) * pointerSize);
-        
-        Logger.VerboseNewline($"Found end of CodeGenModules list at 0x{endOfCodegenModulesList:X}, so start of list is 0x{startOfCodegenModulesList:X}. Returning dummy code reg struct now!");
-
-        //Now we can return a dummy CodeRegistration struct with the correct values.
-        codereg = new() { codeGenModulesCount = (uint)moduleCount, addrCodeGenModulePtrs = startOfCodegenModulesList };
+        return startOfCodegenModulesList;
     }
 }
