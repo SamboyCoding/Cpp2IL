14d95331525173ff04bb033bca3b96403c24c1d8
Sam Byass <sam@samboycoding.me>
2024-08-12 13:53:28 +0100

    Core: Isil: Reduce chance we hit an exception control flow case
    Helps with performance and when debugging with "break on exception" enabled

--- Cpp2IL.Core/ISIL/IsilImmediateOperand.cs
+++ Cpp2IL.Core/ISIL/IsilImmediateOperand.cs
@@ -14,19 +14,22 @@ public readonly struct IsilImmediateOperand : IsilOperandData
 
     public override string ToString()
     {
+        if (Value is string)
+        {
+            return "\"" + Value + "\"";
+        }
+        
         try
         {
-            if (Convert.ToInt64(Value) > 0x1000)
+            //Quick sanity to reduce the possibility of throwing exceptions here, because that's slow
+            var isUlongAndTooLarge = Value is ulong and >= long.MaxValue;
+            
+            if (!isUlongAndTooLarge && Convert.ToInt64(Value) > 0x1000)
                 return $"0x{Value:X}";
         }
         catch
         {
             //Ignore
-        }
-
-        if (Value is string)
-        {
-            return "\"" + Value + "\"";
         }
         
         return Value.ToString(CultureInfo.InvariantCulture);
