4f352999f40ff82eeb85375c12e1f6bd1e913200
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-05-18 22:13:01 -0700

    Return exact type from AssemblyAnalysis::InjectType

--- Cpp2IL.Core/Model/Contexts/AssemblyAnalysisContext.cs
+++ Cpp2IL.Core/Model/Contexts/AssemblyAnalysisContext.cs
@@ -118,7 +118,7 @@ public class AssemblyAnalysisContext : HasCustomAttributesAndName
         }
     }
 
-    public TypeAnalysisContext InjectType(string ns, string name, TypeAnalysisContext? baseType, TypeAttributes typeAttributes)
+    public InjectedTypeAnalysisContext InjectType(string ns, string name, TypeAnalysisContext? baseType, TypeAttributes typeAttributes)
     {
         var ret = new InjectedTypeAnalysisContext(this, ns, name, baseType, typeAttributes);
         InjectType(ret);
