2bd401c0197235092e6376c5503237516ef1b654
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-09-08 14:47:36 -0700

    Missing changes for Release mode

--- Cpp2IL.Core/OutputFormats/AsmResolverDummyDllOutputFormat.cs
+++ Cpp2IL.Core/OutputFormats/AsmResolverDummyDllOutputFormat.cs
@@ -139,7 +139,7 @@ public abstract class AsmResolverDllOutputFormat : Cpp2IlOutputFormat
             catch (System.Exception e)
             {
                 var managedType = typeContext.GetExtraData<TypeDefinition>("AsmResolverType") ?? throw new($"AsmResolver type not found in type analysis context for {typeContext.FullName}");
-                throw new($"Failed to process type {managedType.FullName} (module {managedType.Module?.Name}, declaring type {managedType.DeclaringType?.FullName}) in {context.Name}", e);
+                throw new($"Failed to process type {managedType.FullName} (module {managedType.DeclaringModule?.Name}, declaring type {managedType.DeclaringType?.FullName}) in {context.Name}", e);
             }
 #endif
         }
