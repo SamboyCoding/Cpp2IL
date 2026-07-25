7f25c1da26acef9c35b8cb10f6da26495f827c2e
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2026-07-25 07:28:08 -0700

    Support overriding default values for method parameters (#586)

--- Cpp2IL.Core/Model/Contexts/InjectedFieldAnalysisContext.cs
+++ Cpp2IL.Core/Model/Contexts/InjectedFieldAnalysisContext.cs
@@ -7,13 +7,17 @@ public class InjectedFieldAnalysisContext : FieldAnalysisContext
     public override TypeAnalysisContext DefaultFieldType { get; }
     public override string DefaultName { get; }
     public override FieldAttributes DefaultAttributes { get; }
+    public override int DefaultOffset { get; }
+    public override object? DefaultConstantValue { get; }
 
     protected override bool IsInjected => true;
 
-    public InjectedFieldAnalysisContext(string name, TypeAnalysisContext type, FieldAttributes attributes, TypeAnalysisContext parent) : base(null, parent)
+    public InjectedFieldAnalysisContext(string name, TypeAnalysisContext type, FieldAttributes attributes, TypeAnalysisContext parent, int offset = -1, object? constantValue = null) : base(null, parent)
     {
         DefaultName = name;
         DefaultAttributes = attributes;
         DefaultFieldType = type;
+        DefaultOffset = offset;
+        DefaultConstantValue = constantValue;
     }
 }
